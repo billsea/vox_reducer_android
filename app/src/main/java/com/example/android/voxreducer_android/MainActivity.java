7bd0792a1a48d6e2274247ac9b1b833b8aeb567b
Bill S <bill.e.seaman@gmail.com>
2017-06-19 23:16:15 -0700

    add audio asset player calls

--- app/src/main/java/com/example/android/voxreducer_android/MainActivity.java
+++ app/src/main/java/com/example/android/voxreducer_android/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Filter;
 import android.widget.TextView;
+import android.content.res.AssetManager;
+import android.media.AudioManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    static AssetManager assetManager;
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("reducer");
-
     }
 
     public native String stringFromJNI();
@@ -18,24 +21,32 @@ public class MainActivity extends AppCompatActivity {
     //create reducer
     public static native boolean createReducer();
 
+    public static native boolean createAssetAudioPlayer(AssetManager assetManager, String filename);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        assetManager = getAssets();
+
+        if(createReducer()){
+            //reducer ready
+
+            //create audio player
+            //TODO: add .wav audio resource
+            boolean created = createAssetAudioPlayer(assetManager, "background.mp3");
+        }
+
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
-        if(createReducer()){
-            //reducer ready
-        }
+
 
 
     }
-
-
-
 
 
 
