a9001d8195fa72443b8d6a80d500d0590758d849
Bill S <bill.e.seaman@gmail.com>
2017-06-21 22:58:25 -0700

    move audio_main file to iOS static library so all are in the same place.

--- app/src/main/java/com/example/android/voxreducer_android/MainActivity.java
+++ app/src/main/java/com/example/android/voxreducer_android/MainActivity.java
@@ -11,17 +11,6 @@ public class MainActivity extends AppCompatActivity {
 
     static AssetManager assetManager;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("reducer");
-    }
-
-    public native String stringFromJNI();
-
-    //create reducer
-    public static native boolean createReducer();
-
-    public static native boolean createAssetAudioPlayer(AssetManager assetManager, String filename);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +25,32 @@ public class MainActivity extends AppCompatActivity {
             //create audio player
             //TODO: add .wav audio resource
             boolean created = createAssetAudioPlayer(assetManager, "background.mp3");
-        }
 
+            //play audio
+            setPlayingAssetAudioPlayer(true);
+        }
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
 
-
-
-
     }
+
+
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("reducer");
+    }
+
+    public native String stringFromJNI();
+
+    //create reducer
+    public static native boolean createReducer();
+
+    //audio player
+    public static native boolean createAssetAudioPlayer(AssetManager assetManager, String filename);
+    public static native void setPlayingAssetAudioPlayer(boolean isPlaying);
 
 
 
